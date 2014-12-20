1bfb5ef81b14e024c2cc7d372d5cc35e6106f5a1
Kevin Gillette <extemporalgenome@gmail.com>
2014-12-19 22:51:08 -0700

    Added license comments

--- doc.go
+++ doc.go
@@ -1,3 +1,7 @@
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package size provides byte/bit size and rate constants.
 //
 // All Size and Rate values are internally stored as quanties of bytes.
